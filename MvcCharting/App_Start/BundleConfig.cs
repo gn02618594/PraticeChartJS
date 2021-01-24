0b5b13f3cd85a772d96e8882d57318242f24d61e
gn02618594 <gn02618594@gmail.com>
2021-01-24 22:45:19 +0800

    將 Chart.min.js 註冊到 BundleConfig.cs 在 _Layout.cshtml 佈局檔加入 Chart.js 參考，以及四個RenderSection() 宣告

--- MvcCharting/App_Start/BundleConfig.cs
+++ MvcCharting/App_Start/BundleConfig.cs
@@ -25,6 +25,7 @@ namespace MvcCharting
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+            bundles.Add(new StyleBundle("~/Scripts/Chart.js").Include("~/Scripts/Chart.min.js"));
         }
     }
 }
