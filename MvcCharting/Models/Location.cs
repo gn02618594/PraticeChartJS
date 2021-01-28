354142342dac2bf45327b0d8f27190e70889faea
gn02618594 <gn02618594@gmail.com>
2021-01-28 22:49:56 +0800

    MVC 從 Controller 傳遞資造給 View Json.Encode() 是把 C# 集合或陣列編碼成JSON 反過來是用 Json.Decode()
    編碼後得 JSON 以 Html.Raw() 方法指派給 JavaScript 的陣列或物件

---
+++ MvcCharting/Models/Location.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MvcCharting.Models
+{
+    public class Location
+    {
+       [Display(Name="程式名稱")]
+        public string City { get; set; }
+        [Display(Name ="1-12 月份溫度資料")]
+        public double[] Temperature { get; set; }
+    }
+}
